e5be4ec5879f9646dac81de9c2bed29a29ceb973
Arizki Putra Rahman <arizkijitsu@gmail.com>
2024-01-26 05:37:21 +0700

    changes isOpen = false

--- src/components/Modals/Error.js
+++ src/components/Modals/Error.js
@@ -1,21 +1,22 @@
-import {Modal, ModalBody, ModalContent, ModalOverlay, Text, useDisclosure} from "@chakra-ui/react";
+import { ButtonSpinner, Modal, ModalBody, ModalContent, ModalOverlay, Text, useDisclosure } from "@chakra-ui/react";
+import { MdPause } from "react-icons/md";
 
-const ModalErrorAPI = ()=>{
-    const {onClose } = useDisclosure()
+const ModalErrorAPI = () => {
+  const { onClose } = useDisclosure();
 
-    return (
-        <Modal isCentered isOpen={true} onClose={onClose}>
-            <ModalOverlay
-                bg='blackAlpha.100'
-                backdropFilter='blur(10px) hue-rotate(90deg)'
-            />
-            <ModalContent>
-                <ModalBody bg={'gray.800'} p={8} textAlign={'center'}>
-                    <Text color={'white'} fontFamily={'Lato'} fontSize={'xl'}>API Error, Please Contact Your Administrator</Text>
-                </ModalBody>
-            </ModalContent>
-        </Modal>
-    )
-}
+  return (
+    <Modal isCentered isOpen={true} onClose={onClose}>
+      <ModalOverlay bg="blackAlpha.100" backdropFilter="blur(10px) hue-rotate(90deg)" />
+      <ModalContent>
+        <ModalBody bg={"gray.800"} p={10} textAlign={"center"}>
+          <Text color={"white"} fontFamily={"Lato"} fontSize={"xl"}>
+            <ButtonSpinner color={"gray.200"} />
+            Still Closed, Please Wait ....
+          </Text>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
 
-export default ModalErrorAPI
+export default ModalErrorAPI;
