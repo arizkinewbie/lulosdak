755b8292a0fa93e04b233508f2ef10e454c98840
Arizki Putra Rahman <arizkijitsu@gmail.com>
2024-02-26 11:31:10 +0700

    changes result

--- src/components/Footer/FooterLulus.js
+++ src/components/Footer/FooterLulus.js
@@ -1,14 +1,16 @@
-import {Box, Text} from "@chakra-ui/react";
+import { Box, Text, Button } from "@chakra-ui/react";
 
-const FooterLulus = ()=>{
-    return(
-        <Box p={6}>
-            <Text mb={4} color={'#999'} letterSpacing={'1px'} textAlign={'justify'} fontFamily={'Lato'} fontWeight={'300'} fontSize={'0.8rem'}>Status kelulusan Anda ditetapkan setelah Sekolah melakukan verifikasi data akademik (rapor dan/atau nilai ujian). Silakan Anda membaca peraturan tentang kelulusan siswa.
-            </Text>
-            {/*<Text color={'#999'} letterSpacing={'1px'} textAlign={'justify'} fontFamily={'Lato'} fontWeight={'300'} fontSize={'0.8rem'}>Khusus peserta KIP Kuliah, PTN tujuan juga dapat melakukan verifikasi data ekonomi dan/atau kunjungan ke tempat tinggal Anda sebelum menetapkan status penerimaan Anda.*/}
-            {/*</Text>*/}
-        </Box>
-    )
-}
+const FooterLulus = () => {
+  return (
+    <Box p={6}>
+      <Text mb={4} color={"#999"} letterSpacing={"1px"} textAlign={"justify"} fontFamily={"Lato"} fontWeight={"300"} fontSize={"0.8rem"}>
+        Status kelulusan Anda ditetapkan setelah Sekolah melakukan verifikasi data akademik (rapor dan/atau nilai ujian). Silakan Anda membaca peraturan tentang kelulusan siswa.
+      </Text>
+      <Button colorScheme={"blue"} onClick={() => window.history.back()} _hover={{ bg: "white", color: "blue.500" }}>
+        Kembali
+      </Button>
+    </Box>
+  );
+};
 
-export default FooterLulus
+export default FooterLulus;
